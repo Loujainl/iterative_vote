7deeeb752500da1ae194baa0be9814f74c899406
Loujainl <lujayn.layka@gmail.com>
2019-05-21 10:25:04 +0200

    cleaning

--- iterative_vote.py
+++ iterative_vote.py
@@ -16,10 +16,9 @@ def find_majority(votes):
 
 
 def create_profiles(voters_num, quest_num):
-            # create agents
+            # create game
     agents = []
     for i in range(voters_num):
-        # name = 'agent-' + str(i+1)
         agent = np.reshape(list(itertools.product([0, 1], repeat = quest_num)), (2**quest_num,quest_num))
         np.random.shuffle(agent)
         agents.append((agent))
@@ -37,65 +36,15 @@ def create_profiles(voters_num, quest_num):
             self.observation_space = spaces.Discrete(1)
             self.reward_range = (0,2**quest_num - 1)
 
-            if test:
-              self._seed(voters_num)
-            else:
-              self._seed()
-
-            # Start the first game
-            self.reset()
-
-        def _seed(self, seed=None):
-            self.np_random, seed = seeding.np_random(seed)
-            return [seed]
-
-
-        #fix seed to get the same result everytime
-        np.random.seed(voters_num)
-        # generate the permutations
-        profile1 = np.reshape(list(itertools.product([0, 1], repeat = quest_num)), (2**quest_num,quest_num))
-        np.random.shuffle(profile1)
-        print("Profile one:",profile1)
-        # [[1 0 1]
-        #  [1 1 1]
-        #  [1 0 0]
-        #  [1 1 0]
-        #  [0 1 1]
-        #  [0 0 1]
-        #  [0 0 0]
-        #  [0 1 0]]
-        profile2 = np.reshape(list(itertools.product([0, 1], repeat = quest_num)), (2**quest_num,quest_num))
-        np.random.shuffle(profile2)
-        print("Profile two:",profile2)
-
-        profile3 = np.reshape(list(itertools.product([0, 1], repeat = quest_num)), (2**quest_num,quest_num))
-        np.random.shuffle(profile3)
-        print("Profile three:",profile3)
-
 
         def _get_obs(self):
             # return (sum_hand(self.player), self.dealer[0], usable_ace(self.player))
             return 1
 
         def reset(self):
-            # self.dealer = draw_hand(self.np_random)
-            # self.player = draw_hand(self.np_random)
+
             return self._get_obs()
 
-
-        # class Actions(enum.IntEnum):
-        #     """ Actions for the player """
-        #     SINCERE = 0
-        #     LEARN = 1
-        #
-        # class Action_Mode(enum.IntEnum):
-        #     INITIAL = 0
-        #     TRAIN = 1
-
-
-
-        # collectivote = np.concatenate(profile1[1], profile2[1], profile3[1])
-        # print("Collective vote",collectivote)
 
 x = create_profiles(3,3)
 print(x[0][0],x[1][0], x[2][0] )

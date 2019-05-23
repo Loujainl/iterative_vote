50e266e0b6fdcc2a90de92d5492677d1027147d0
Loujainl <lujayn.layka@gmail.com>
2019-05-23 17:17:01 +0200

    Enhance

--- iterative_vote.py
+++ iterative_vote.py
@@ -8,15 +8,16 @@ import numpy as np
 import enum
 
 
-
 def find_majority(votes):
 
-    """votes is 1-d array, per question"""
-    bincnt =np.bincount(votes)
-    print("number of zeros = ", bincnt[0])
-    print("number of ones = ", bincnt[1])
-    return 1 if bincnt[1]>bincnt[0] else 0
+    """votes is 2-d array, (actions submitted) """
 
+    bincnt =np.apply_along_axis(lambda x: np.bincount(x, minlength=2), 0, arr=votes)
+    # ("number of zeros = ", bincnt[0])
+    # ("number of ones = ", bincnt[1])
+    q = bincnt.shape[1]
+    # print(bincnt,q)
+    return [1 if bincnt[1][i]>bincnt[0][i] else 0 for i in range(q)]
 
 def create_profiles(voters_num, quest_num):
 
@@ -33,27 +34,26 @@ def create_profiles(voters_num, quest_num):
     return agents
 
 profiles = create_profiles(3,3)
+print(profiles)
+sincere_actions = [profiles[i][0] for i in range(len(profiles))]
 def calculate_result(actions):
 
     """  (selectedaction) returns majority vote result  """
-    actions = [profiles[0][0],profiles[1][0], profiles[2][0]]
-    print(actions)
-    result = []
-    for i in range(len(actions[0])):
-        question = [actions[0][i], actions[1][i], actions[2][i] ]
-        print("question",str(i+1), question)
-        per_question = find_majority(question)
-        result.append(per_question)
-    print(result)
+    # actions =np.array([profiles[0][0],profiles[1][0], profiles[2][0]])
+    # act = np.array([profiles[0][0],profiles[1][0], profiles[2][0]])
+    # print(actions, " << Actions")
+    result = find_majority(actions)
+    print("MV:" ,result)
     return result
 
-calculate_result(profiles)
+calculate_result(sincere_actions)
 
 
 
 class IterativeVote(gym.Env):
     voters_num= 3 #agents, learners
     quest_num = 3  #multiple proposals
+    profile = create_profiles(voters_num, quest_num)
 
     def __init__(self, voters_num=3,  quest_num = 3, test=False):
         self.action_space = spaces.Discrete(2**quest_num)
@@ -70,3 +70,6 @@ class IterativeVote(gym.Env):
 
 
 
+    def step(self, actions):
+        next_obs = calculate_result(actions)
+        return next_obs
